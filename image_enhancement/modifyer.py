4ce5346352b70b3970ceb96fea76cc8b33f7dd98
Jose Luis Gonzalez <49054211+josegzl@users.noreply.github.com>
2024-07-22 11:33:53 -0600

    code corrections

--- image_enhancement/modifyer.py
+++ image_enhancement/modifyer.py
@@ -2,7 +2,7 @@ import cv2
 
 image= cv2.imread('//home//jose//Downloads//image_enhancement//images//image1.jpg')
 
-cv2.line(image, (0,0),(3500,4500),(0,255,0),30)
+cv2.line(image, (0,0),(3500,4500),(0,255,0),20)
 
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
